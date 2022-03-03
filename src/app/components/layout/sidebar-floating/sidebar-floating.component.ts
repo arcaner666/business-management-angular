e7505378bb34d8f8b09b5bc6be67d0f27a8a1665
Arcan Caner Şenol <arcaner666@gmail.com>
2022-03-03 10:56:08 +0300

    updated

--- src/app/components/layout/sidebar-floating/sidebar-floating.component.ts
+++ src/app/components/layout/sidebar-floating/sidebar-floating.component.ts
@@ -1,10 +1,13 @@
 import { Component, OnInit } from '@angular/core';
+import { Router } from '@angular/router';
 
 import { Observable } from 'rxjs';
 
+import { AuthorizationDto } from 'src/app/models/dtos/authorizationDto';
 import { LayoutConfig } from 'src/app/models/various/layout-config';
 import { NavGroup } from 'src/app/models/various/nav-group';
 
+import { AuthorizationService } from 'src/app/services/authorization.service';
 import { BreakpointService } from 'src/app/services/breakpoint.service';
 import { LayoutService } from 'src/app/services/layout.service';
 import { NavigationService } from 'src/app/services/navigation.service';
@@ -16,19 +19,29 @@ import { NavigationService } from 'src/app/services/navigation.service';
 })
 export class SidebarFloatingComponent implements OnInit {
 
+  public authorizationDto$: Observable<AuthorizationDto>;
   public sidebarLinks$: Observable<NavGroup[]>;
   public layoutConfig$: Observable<LayoutConfig>;
 
   constructor(
+    private _authorizationService: AuthorizationService,
     private _navigationService: NavigationService,
     private _layoutService: LayoutService,
+    private _router: Router,
 
     public breakpointService: BreakpointService
   ) {
     this._navigationService.loadSidebarLinksByRole();
 
-    this.layoutConfig$ = this._layoutService.layoutConfigObservable
+    this.authorizationDto$ = this._authorizationService.authorizationDtoObservable;
+    this.layoutConfig$ = this._layoutService.layoutConfigObservable;
     this.sidebarLinks$ = this._navigationService.sidebarLinks$;
+  }
+
+  logout() {
+    this._authorizationService.clearAuthorizationDto();
+    this._navigationService.loadSidebarLinksByRole();
+    this._router.navigate(["public/home"]);
   }
 
   toggleSidebarFloating() {
