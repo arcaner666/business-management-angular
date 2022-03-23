b7091317bb5f6b39efafe2674653bdb9af696e78
Arcan Caner Şenol <arcaner666@gmail.com>
2022-03-23 14:10:44 +0300

    updated

--- src/app/services/manager.service.ts
+++ src/app/services/manager.service.ts
@@ -1,37 +1,12 @@
 import { Injectable } from '@angular/core';
 import { HttpClient } from '@angular/common/http';
 
-import { BehaviorSubject, Observable } from 'rxjs';
-import { cloneDeep } from 'lodash';
+import { Observable } from 'rxjs';
 
 import { environment } from 'src/environments/environment';
 
 import { ListDataResult } from 'src/app/models/results/list-data-result';
 import { ManagerExtDto } from 'src/app/models/dtos/manager-ext-dto';
-import { Result } from 'src/app/models/results/result';
-
-const EMPTY_MANAGER_EXT_DTO: ManagerExtDto = {
-  managerId: 0,
-  businessId: 0,
-  branchId: 0,
-  nameSurname: "",
-  email: "",
-  phone: "",
-  dateOfBirth: new Date(),
-  gender: "",
-  notes: "",
-  avatarUrl: "",
-  createdAt: new Date(),
-  updatedAt: new Date(),
-
-  // Extended With Business
-  businessName: "",
-
-  // Extended With Branch + FullAddress
-  cityId: 0,
-  districtId: 0,
-  addressText: "",
-};
 
 @Injectable({
   providedIn: 'root'
@@ -45,10 +20,6 @@ export class ManagerService {
   ) {}
 
   // API İstekleri
-  addSectionManager(managerExtDto: ManagerExtDto): Observable<Result> {
-    return this._http.post<Result>(`${environment.apiUrl}/${this.controllerUrl}/addsectionmanager`, managerExtDto);
-  }
-
   getExtsByBusinessId(businessId: number): Observable<ListDataResult<ManagerExtDto>> {
     return this._http.get<ListDataResult<ManagerExtDto>>(`${environment.apiUrl}/${this.controllerUrl}/getextsbybusinessid/${businessId}`);
   }
